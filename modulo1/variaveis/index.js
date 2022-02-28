e83958410dab98e8939efc1924abe40c92f00e3f
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-02-27 21:12:57 -0300

    variaveis-finalizada

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,4 +1,4 @@
-//TAREFA DO ZERO 
+/*TAREFA DO ZERO 
 
 let a = 10
 let b = 10
@@ -8,12 +8,12 @@ console.log(b)
 b = 5
 console.log(a, b) 
 
-/**comentário: é possível observar que na primeira situacao a = 10 e b = 10 
+comentário: é possível observar que na primeira situacao a = 10 e b = 10 
 o console irá imprimir esses valores 
 Algo muda onde b recebe um novo valor no caso 5 então b = 5 o console vai imprimir
 
 a = 10, b = 5 
-**/
+
 let a = 10
 let b = 20
 c = a
@@ -21,8 +21,8 @@ b = c
 a = b
 
 console.log(a, b, c)
-/**No primeiro momento a = 10 e b = 20 logo abaixo c vai ser igual a letra a, portanto
-a = 10 e c = 10 e b = c  sendo assim b = 10 **/
+No primeiro momento a = 10 e b = 20 logo abaixo c vai ser igual a letra a, portanto
+a = 10 e c = 10 e b = c  sendo assim b = 10 
 
 
 let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
@@ -34,8 +34,7 @@ const nome = prompt ("Qual é o seu nome?")
 let idade = prompt ("Qual sua idade?")
 
 console.log (typeof nome, typeof idade) 
-/**Comentário Aqui foi usado o comando typeof para identificar
-a string usada refere-se à texto.**/
+//Comentário Aqui foi usado o comando typeof para identificar a string usada refere-se à texto.
 
 
 const nome = prompt ("Qual é o seu nome?")
@@ -47,8 +46,50 @@ console.log (idade, "34 anos");
 
 const nome = prompt ("Qual é o seu nome?")
 let idade = prompt ("Qual sua idade?")
+console.log ("Olá meu nome é,", nome, "você tem", idade, "anos.")
 
 console.log (typeof nome, "Alessandra");
 console.log (typeof idade, "34 anos");
 
-//COMENTÁRIO: AQUI NO CASO IMPRIME AS STRINGS 
+letra d - É importante identificar a o tipo da variável para ter coerência com o que se esta sendo programado, texto usa String, números Number e Boleano  para casos de false ou true.
+
+EXERCÍCIO 2 
+const cor = ("Você está usando blusa amarela?")
+let souUmBoolean = false
+
+const cor1 = ("Você está usando uma calça verde?")
+let souUmBoolean1 = false
+
+const cor2 = ("Você está usando vestido?")
+let souUmBoolean2 = true
+
+console.log(cor, souUmBoolean, cor1, souUmBoolean1, cor2, souUmBoolean2)
+
+let a = 10
+let b = 25
+
+c = a // c = 10
+a = b // a = 25 
+b = c // b = 10 
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+
+let recebeUsuario = Number (prompt("Digite um números"))
+let recebeUsuario2 = Number (prompt("Digite outro números"))
+
+let soma = (recebeUsuario + recebeUsuario2)
+let multiplicacao = (recebeUsuario * recebeUsuario2)
+
+console.log(soma)
+console.log(multiplicacao)*/
+
+
+
+
+
+
+
+
+
+
