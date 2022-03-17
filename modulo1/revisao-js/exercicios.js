62e7f45a263f7ed265777151ed3414640133b9a3
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-03-16 21:20:00 -0300

    revisao-js reiniciando exercicios

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -4,26 +4,21 @@
 
 
 // EXERCÍCIO 01
-
-const array = ["azul", "amarelo", "vermelho"]
-
+let array = [3, 2, 1, 4, 7]
 function retornaTamanhoArray(array) {
     return array.length
-    
+       
 }
-console.log([array.length])
 
 // EXERCÍCIO 02
-
-const numeros = [0,8,23,16,10,15,41,12,13]
-
+let array2 = [8, 23, 16, 10]
 function retornaArrayInvertido(array) {
-     return array.reverse();
+    return array.reverse()
+  
 }
 
 // EXERCÍCIO 03
-
-let array = [36,12,56,7,3]
+let array3 = [36,12,56,7,3]
 function comparar(a, b) {
     if (a < b) {
       return -1;
@@ -37,10 +32,8 @@ function retornaArrayOrdenado(array) {
     return array.sort(comparar);
 }
 
-
 // EXERCÍCIO 04
 let array = [1, 2, 3, 4, 5, 6, 7, 8]
-
 function retornaNumerosPares(array) {
     let arrayNovo = []
 
@@ -49,25 +42,23 @@ function retornaNumerosPares(array) {
             arrayNovo.push(array[i])
         }
     return arrayNovo
-}   
+    
+  
+}
 
 // EXERCÍCIO 05
-
 let array = [1, 2, 3, 4, 5, 6]
 function retornaNumerosParesElevadosADois(array) {
-    let arrayNovo = []
-    for (var i = 0; i < 6; i++)
-        if (array[i] % 2 == 0){
-            arrayNovo.push(array[i] **2)
-        }
-    return arrayNovo 
+    const max = Math. max(...array);
+    return max
 }
+
 
 // EXERCÍCIO 06
 let array = [1, 5, 3, 7, 5, 2]
 function retornaMaiorNumero(array) {
-    const max = Math. max(...array);
-    return max
+    const max = Math.max
+
 }
 
 // EXERCÍCIO 07
@@ -81,14 +72,7 @@ function retornaNPrimeirosPares(n) {
 }
 
 // EXERCÍCIO 09
-let escaleno = [1, 2, 4]
-let isosceles = [1, 2, 1]
-let equilatero = [5, 5, 5]
-
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-   const calculo = ladoA * ladoB / ladoC
-    if (ladoA == ladoB && ladoB == ladoC) {
-        "Triangulo Equilátero"
 
 }
 
