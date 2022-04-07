e789d88ed77bcb0ae3132d6c1666baf6074d28f2
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-04-06 22:08:27 -0300

    apis.labenusers-botao-nao-volta

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import styled from 'styled-components'
+import styled from 'styled-components';
 import TelaCadastro from "./components/TelaCadastro";
 import TelaListaUsuarios from "./components/TelaListaUsuarios";
 
--- modulo2/apis-labenusers/apis-labenusers/src/components/DetalheDoUsuario.js
+++ modulo2/apis-labenusers/apis-labenusers/src/components/DetalheDoUsuario.js
@@ -1,19 +1,38 @@
-import React from 'react'
-import axios from 'axios'
-import styled from 'styled-components'
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 export default class DetalheDoUsuario extends React.Component {
-    state = {
-        DetalhesDoUsuario: {}
-    }
-    render(){
-        return(
-            <div>
-                <button>Deletar</button>
-            </div>
-        )
-    }
-}
+  state = {
+    detalhesDoUsuario: {},
+  };
+  componentDidMount() {
+    this.detalhes();
+  }
 
-   
-  
+  detalhes = () => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${this.props.identificacaoUsuario}`;
+    axios
+      .get(url, {
+        headers: {
+          Authorization: "alessandra-sandeski-silveira",
+        },
+      })
+      .then((res) => {
+        this.setState({ detalhesDoUsuario: res.data });
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+  render() {
+    return (
+      <div>
+        <p>{this.state.detalhesDoUsuario.name}</p>
+        <p>{this.state.detalhesDoUsuario.email}</p>
+
+        <button onClick={this.state.trocandoPagina}>Página Anterior</button>
+      </div>
+    );
+  }
+}
