f7018ecbc2a6f63ef69be4192add00c271f43165
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-05-24 23:08:39 -0300

    Desafio

--- modulo4/node-packge-jason/index.js
+++ modulo4/node-packge-jason/index.js
@@ -1,19 +1,26 @@
 // letra (a) Vamos acessar essa informação através de uma const que registra a propriedade process.argv[2]. 
 //Exercício 1 
+
+//DESAFIO
 const nome = (process.argv[2])
-const idade = (process.argv[2])
+const idade = Number (process.argv[2])
 
-console.log(nome === 'Alessandra' && idade === 35 ? "Nome correto" : "Nome errado, digite novamente por favor!")
+console.log(nome === (process.argv[2]) && idade === (process.argv[2]) ? "Nome correto" : "Esperava 2 parâmetros só recebi um, tente novamente por favor!"); 
+
+var red, blue, reset;
+red   = '\u001b[31m';
+blue  = '\u001b[34m';
+reset = '\u001b[0m';
+console.log(red +"Aqui esta o texto em vermelho. "+ blue +"Aqui esta o texto em vermelho. "+ reset +"Aqui estamos dando reset nas cores do bash.")
 
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}.`)
+// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}.`)
 
 
+// const tarefa = ['passear com Thor'];
+// tarefa.push(process.argv[2])
 
-const tarefa = ['passear com Thor'];
-tarefa.push(process.argv[2])
-
-console.log(tarefa)
+// console.log(tarefa)
 
 //exercício 2 criar pastas e dividir o exercicio ok 
 
