59c20af88c1208509bfdaedf25f34b05dd3cd5e8
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-05-25 21:44:49 -0300

    desafio 1

--- modulo4/intro-typescript/build/index6.js
+++ modulo4/intro-typescript/build/index6.js
@@ -1,8 +1,9 @@
 "use strict";
 function recebeNumero(n1, n2) {
-    const primeiroNumero = Number(process.argv[2]);
-    const segundoNumero = Number(process.argv[3]);
-    return (primeiroNumero + segundoNumero);
+    console.log(n1 + n2);
+    console.log(n1 - n2);
+    console.log(n1 * n2);
+    console.log(Math.max(n1, n2));
 }
-console.log(recebeNumero(20, 40));
+recebeNumero(6, 2);
 //# sourceMappingURL=index6.js.map
--- modulo4/intro-typescript/src/index6.ts
+++ modulo4/intro-typescript/src/index6.ts
@@ -1,19 +1,17 @@
-function recebeNumero (n1:number, n2:number):number{
-    const somar = (primeiroNumero + segundoNumero)
-    const multiplicar = (primeiroNumero * segundoNumero)
-    const subtracao = (primeiroNumero / segundoNumero)
-
-    
-    // const primeiroNumero:number = Number(process.argv[2]);
-    // const segundoNumero:number =  Number(process.argv[3]);
-    // const segundoNumero:number =  Number(process.argv[4]);
-    return(
-        
-
-    )
+function recebeNumero (n1:number, n2:number):void{
+   console.log(n1 + n2)
+    console.log(n1 - n2)
+    console.log(n1 * n2)
+    console.log(Math.max(n1, n2))
+    //Math.max verifica qual dos números que estou passando para a função é o maior número. 
 
 }
+recebeNumero (6,2)
 
-console.log (recebeNumero(primeiroNumero + segundoNumero))
+
 //dar um console log na soma deles, outro console.log na subtração, outro na multiplicação e outro no maior numero, no final de tudo vai chamar a função passando os parametros(os dois numeros)
+
+function convert = (DNA:string):any => {
+    const recebeDna:string = "ATTGCTGCGCATTAACGACGCGTA"
+}
 
