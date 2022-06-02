f24ce9d623bbcb8919e519c0737c02b33c07a93d
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-06-02 19:18:19 -0300

    apis rest

---
+++ modulo4/apis-rest/src/index.ts
@@ -0,0 +1,111 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { users } from './data/types'
+import { send } from "process";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/users", (req: Request, res: Response) => {
+    let codeError: number = 400;
+
+    try {
+        if (users) {
+            res.status(200).send(users)
+        } else {
+            codeError = 404
+            throw new Error('Lista de usuário não encontrada')
+        }
+
+    } catch (error: any) {
+        res.status(codeError).send({ message: error.message })
+    }
+
+})
+
+app.get("/users/:type", (req: Request, res: Response) => {
+    let codeError: number = 400;
+
+    try {
+        const type = req.params.type
+        const filtrando = users.filter((user) => {
+            if (user.type === type) {
+                return user
+            }
+        })
+
+        if (!type) {
+            codeError = 404
+            throw new Error('É preciso digitar algum parametro')
+
+        }
+        if (!filtrando.length) {
+            codeError = 404
+            throw new Error('Lista de usuário não encontrada')
+        }
+        res.status(200).send(filtrando)
+
+    } catch (error: any) {
+        res.status(codeError).send({ message: error.message })
+
+    }
+
+})
+
+app.get("/users2", (req: Request, res: Response) => {
+    let codeError: number = 400;
+
+    try {
+        const name = (req.query.name) as string
+        const filtrandoUsuario: users | undefined = users.find(user => user.name === name)
+
+        if (!filtrandoUsuario) {
+            codeError = 404
+            throw new Error('Usuário não encontrado por favor digite um nome de usuário!')
+        }
+
+        res.status(200).send(filtrandoUsuario)
+    } catch (error: any) {
+        res.status(codeError).send({ message: error.message })
+
+    }
+
+
+})
+
+app.put("/users", (req: Request, res: Response) => {
+    let codeError: number = 400;
+
+    try {
+        const { id, name, email, type, age } = req.body
+
+        if (!id || !name || !email || !type || !age) {
+            let errorCode = 422
+            throw new Error('Por favor verifique se os campos foram preenchidos corretamente')
+        }
+
+        const newUser: users = {
+            id,
+            name,
+            email,
+            type,
+            age
+        }
+
+       users.push(newUser)
+       res.status(201).send({message:'Usuário criado com sucesso!', users})
+
+    } catch (error: any) {
+        res.status(codeError).send({ message: error.message })
+    }
+
+})
+
+
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+});
---
+++ modulo4/apis-rest/src/respostas.ts
@@ -0,0 +1,31 @@
+//Exercicio 01 
+
+// Resposta a.) Vou utilizar o método Get para pegar a lista de Usuário dentro do Array de objeto. 
+
+// Resposta b.) A entidade utilizada neste Array foi uma lista de Usuários. 
+
+//Exercicio 02
+
+// Resposta a.) Passei o parâmetro direto dentro da requisição. Porque se eu estou buscando pelo type isso torna a busca "específica". 
+
+// Resposta b.) Não criei o ENUM, acabei fazendo o ENUM após a lógica ficar pronta, por esse motivo não usei ele na comparação. Mas se usar na função a comparação poderia se basear nos dados que estão nele. 
+
+//EXERCÍCIO 03 
+
+// Resposta a.) O TYPE do envio de parâmetro utilizado foi o Query por ser uma busca específica do usuário dentro do Array. 
+
+// Resposta b.) Ok feito no exercício!
+
+//EXERCÍCIO 04 
+
+
+// Resposta a.) Troquei do método post para o PUT e observei que nada mudou. Acredito que não tenha problema em usar esse método. 
+
+//Resposta b.) De acordo com o conteúdo didático o método PUT atualiza os recursos cria caso não exista, mas para criar um usuário novo existe o método POST. Cada método deve ser usado da forma correta. Minha resposta pessoal seria utilizar a forma que a método POST procede. 
+
+
+
+
+
+
+
