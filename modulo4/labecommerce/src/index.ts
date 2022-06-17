259191842c8dbdf86f230f69c7069bebaa157283
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-06-17 17:14:23 -0300

    labecommerce

---
+++ modulo4/labecommerce/src/index.ts
@@ -0,0 +1,92 @@
+import express, { Express, Request, Response, } from "express";
+import cors from "cors";
+import { connection } from "./data/connection";
+
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+//Criando o usuário - função assíncrona utilizando async e await, body, uma resposta de sucesso. 
+
+app.post("/users", async (req:Request, res:Response)=>{
+    try {
+        const {name, email, password} = req.body
+                if(!name || !email || !password){
+            throw new Error ("Por gentileza informe os campos corretamente")
+        }
+       await connection("labecommerce_users")
+       .insert({
+        id: Date.now().toString(),
+        name,
+        email,
+        password
+       });
+       res.status(200).send({message:"Usuário criado com sucesso!"})
+    } catch (error:any) {
+        res.status(500).send(error.sqlMessage || error.message)
+    }
+})
+
+
+// Busca por todos os usuários!
+
+app.get("/users", async (req:Request, res:Response)=>{
+    try {
+        const table = await connection("labecommerce_users")
+        res.status(200).send({table})
+    } catch (error:any) {
+        res.status(500).send(error.sqlMessage || error.message)
+    }
+
+})
+
+// CRIAR PRODUTO 
+
+app.post("/products", async (req:Request, res:Response)=>{
+    try {
+        const {name, price, image_url} = req.body
+                if(!name || !price || !image_url){
+            throw new Error ("Por gentileza informe os campos corretamente")
+        }
+       await connection("labecommerce_products")
+       .insert({
+        id: Date.now().toString(),
+        name,
+        price,
+        image_url
+       });
+       res.status(200).send({message:"Produto criado com sucesso!"})
+    } catch (error:any) {
+        res.status(500).send(error.sqlMessage || error.message)
+    }
+})
+
+// PEGAR OS PRODUTOS 
+
+app.get("/products", async (req:Request, res:Response)=>{
+    try {
+        const table = await connection("labecommerce_products")
+        res.status(200).send({table})
+    } catch (error:any) {
+        res.status(500).send(error.sqlMessage || error.message)
+    }
+
+})
+
+// REGISTRO DE COMPRAS 
+
+
+
+
+
+
+
+
+
+
+
+
+const server = app.listen(3003, () => {
+       console.log(`Server is running in http://localhost:3003`);
+});
