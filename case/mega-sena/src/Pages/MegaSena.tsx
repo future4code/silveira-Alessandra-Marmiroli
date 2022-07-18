2fb8e0352b6f20bf2ee5073f437aa9f1e51d69fe
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-07-18 18:36:35 -0300

    Mega Sena-estruturando

---
+++ case/mega-sena/src/Pages/MegaSena.tsx
@@ -0,0 +1,8 @@
+import react from "react"
+
+const MegaSena = () => {
+    return (
+       <div>Mega Sena</div>
+    )
+}
+export default MegaSena
---
+++ case/mega-sena/src/Routes/Router.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MegaSena from "../Pages/MegaSena";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes> <Route index element = {MegaSena}/></Routes>
+    </BrowserRouter>
+  )
+}
+
